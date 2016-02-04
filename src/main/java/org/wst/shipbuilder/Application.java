72fd0aa1934cb77d2718146e701884b94bca96df
john <john@thegranges.co.uk>
2016-02-04 21:49:47 +0000

    updates

--- src/main/java/org/wst/shipbuilder/Application.java
+++ src/main/java/org/wst/shipbuilder/Application.java
@@ -1,10 +1,16 @@
 package org.wst.shipbuilder;
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.wst.shipbuilder.data.InvTypeDAO;
 import org.wst.shipbuilder.data.ItemManufactureDAO;
 
@@ -31,6 +37,29 @@ public class Application {
     	dataSource.setPassword("shipmaker");
     	return dataSource;
     }
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+
+      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      vendorAdapter.setGenerateDdl(true);
+
+      LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+      factory.setJpaVendorAdapter(vendorAdapter);
+      factory.setPackagesToScan("org.wst.shipbuilder.data.entities");
+      factory.setDataSource(dataSource());
+      factory.afterPropertiesSet();
+
+      return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+
+      JpaTransactionManager txManager = new JpaTransactionManager();
+      txManager.setEntityManagerFactory(entityManagerFactory());
+      return txManager;
+    }
+    
     @Bean
     public ItemManufactureDAO itemManufactureDAO() {
     	return new ItemManufactureDAO();
