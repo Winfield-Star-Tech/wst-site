54a7d6c4afc2f1c90986739142b09ad8205a0137
john <john@thegranges.co.uk>
2016-04-09 15:19:05 +0100

    parsing db url

--- src/main/java/org/wst/shipbuilder/DefaultPageController.java
+++ src/main/java/org/wst/shipbuilder/DefaultPageController.java
@@ -8,9 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Configuration
 public class DefaultPageController {
+	private int donations = 0;
 	@RequestMapping("/")
 	public String greeting(Model model) {
 		return "index";
 	}
-
+	
+	@RequestMapping("/toshuu")
+	public String toshuu(Model model) {
+		return "toshuu";
+	}
+	@RequestMapping("/operation-hephaestus")
+	public String hephaestus(Model model) {
+		model.addAttribute("donations", donations + (1E7 * Math.random()));
+		return "Hephaestus";
+	}
+	@RequestMapping("/ship-orders")
+	public String shipOrders(Model model) {
+		return "ship-orders";
+	}	
 }
