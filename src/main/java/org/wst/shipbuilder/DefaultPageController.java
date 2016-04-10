6b2198126dcb409c33c1125584377dee1b6b27c4
Kalfar <kalfar.wst@gmail.com>
2016-04-10 21:26:33 +0100

    working with simple form authentication

--- src/main/java/org/wst/shipbuilder/DefaultPageController.java
+++ src/main/java/org/wst/shipbuilder/DefaultPageController.java
@@ -1,9 +1,16 @@
 package org.wst.shipbuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Configuration
@@ -27,4 +34,17 @@ public class DefaultPageController {
 	public String shipOrders(Model model) {
 		return "ship-orders";
 	}	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}	
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+	}
 }
