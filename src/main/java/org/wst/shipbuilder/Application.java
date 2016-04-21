7ce4614b770c37fe4baa57327cb55df7fc2b2f85
john <john@thegranges.co.uk>
2016-04-21 22:59:48 +0100

    merged wst-site with redhat site

---
+++ src/main/java/org/wst/shipbuilder/Application.java
@@ -0,0 +1,57 @@
+package org.wst.shipbuilder;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+
+@SpringBootApplication
+@Configuration
+@EnableJpaRepositories
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	dataSource.setDriverClassName("org.postgresql.Driver");
+    	dataSource.setUrl("jdbc:postgresql://localhost:5432/wst-site");
+    	dataSource.setUsername("wst-site");
+    	dataSource.setPassword("wst-site");
+    	return dataSource;
+    }
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+
+      HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      vendorAdapter.setGenerateDdl(true);
+
+      LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+      factory.setJpaVendorAdapter(vendorAdapter);
+      factory.setPackagesToScan("org.wst.shipbuilder.data.entities");
+      factory.setDataSource(dataSource());
+      factory.afterPropertiesSet();
+
+      return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+
+      JpaTransactionManager txManager = new JpaTransactionManager();
+      txManager.setEntityManagerFactory(entityManagerFactory());
+      return txManager;
+    }
+   
+}
